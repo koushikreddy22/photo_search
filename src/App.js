8b5f30606334e0680198035f8e63f52ada8abd76
koushik <koushikmuni2@gmail.com>
2023-02-10 20:01:05 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [bookmarks,setBookmark]=useState([])
+  const [search,setSearch]=useState("")
+  const [images,setImages]=useState([])
+  const url="https://api.unsplash.com/photos/?client_id="
+  const Access_key="Hyw3O6ipt1PO84ktmJdXDXpviDxkmcfpcQNybmtORmE"
+  const secret_key="vCeinKh_wv8HmWBG_OGPxVHYWHIXp_bkmAPcUH0vSUI"
+  const data=fetch(url+Access_key,{
+    params:{
+      query:"apple"
+    }
+  })
+  useEffect(()=>{data.then((res)=>res.json()).then((result)=>{
+    let arr=[]
+    console.log(result)
+    result.map((item)=>arr.push(item.urls.small_s3))
+    setImages(arr)
+    // console.log(arr)
+  }).catch(err=>console.log(err))},[])
+ console.log(bookmarks)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='main'>
+      <header>
+        <h1>React Photo Search</h1>
+        <button className='bookmarks' onClick={()=>(setImages(bookmarks))}>Bookmarks</button>
       </header>
+      <div className='search-container'>
+        <input onChange={(e)=>{setSearch(e.target.value)}} type="text" placeholder='    Search free high Resolution Images'/>
+        <button className='search'>Search</button>
+      </div>
+      <h5>Bookmarks icons on click will bookmark respective images</h5>
+      <div className='img-container'>{
+       images.map(item=><div><img  className='image' src={item}/> <div onClick={()=>{setBookmark([...bookmarks,item])}} className='bookmark'>🔖</div></div>)
+      }</div>
     </div>
-  );
+    )
 }
 
 export default App;
